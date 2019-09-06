13afb67c7dca8a922335f1a2af1a8de30a994282
Arlid Henao Rueda <arliid@hotmail.com>
2019-09-05 21:17:39 -0500

    [update] model Api/User

--- apis/models.py
+++ apis/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
+class User(models.Model):
 
     # Definimos los campos del modelo
     iduser = models.AutoField(primary_key = True)
     name_user = models.CharField(max_length = 254)
     email_user = models.CharField(max_length = 254)
+    phone_user = models.CharField(max_length = 20, default = '')
 
 
     def __str__(self):
--- apis/urls.py
+++ apis/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
+from . import serializers
 
 urlpatterns = [
     path('', views.holamundo)
